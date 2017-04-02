347d6a6da02e710cfbc7c762fff5bbabea91668d
Samuli Rouvinen <samrouvi@dhcp-asv-112.helsinki.fi>
2017-04-02 22:48:35 +0300

    Tehty tehtävä 2

--- gradleprojekti/src/main/java/Main.java
+++ gradleprojekti/src/main/java/Main.java
@@ -4,10 +4,12 @@ import ohtu.Multiplier;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Multiplier kolme = new Multiplier(3);
+        final int mul = 3;
+        Multiplier kolme = new Multiplier(mul);
         System.out.println("anna luku ");
         int luku = scanner.nextInt();
 
         System.out.println("luku kertaa kolme on "+kolme.multipliedBy(luku) );
+
     }
 }
